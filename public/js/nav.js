94a5683e3d5c5ae5492927bd5a53e5f8de4d0b02
Roland John P. Gracias <rolandgracias26@gmail.com>
2023-12-01 23:32:58 +0800

    Developed the front end of the create student tab. Changes to the href of the nav bar to redirect to the correct pages when clicked.

--- public/js/nav.js
+++ public/js/nav.js
@@ -3,11 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const rightNav = document.querySelector('.right-nav');
     const rightNavTop = document.querySelector('.right-nav-top');
 
+    rightNav.classList.add('show');
+
     navToggle.addEventListener('click', function () {
         rightNav.classList.toggle('show');
     });
 
-    // Close the right navigation when clicking on an element inside .right-nav-top
     rightNavTop.addEventListener('click', function () {
         rightNav.classList.remove('show');
     });
